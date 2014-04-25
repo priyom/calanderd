87ab3a7928fc523230e9a7eaff26c8e8cc9f54c1
Tomáš Hetmer <TomHetmer@users.noreply.github.com>
2014-04-25 21:55:16 +0200

    this didn't work

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ config.server = 'kornbluth.freenode.net';
 config.botName = 'MaciejHunia';
 
 config.calendarId = 'ul6joarfkgroeho84vpieeaakk' // this is in your iCal, html, etc. URLs
-config.announceEarly = (2 * 60000) - 20000; // milliseconds
+config.announceEarly = 2 * 60000; // milliseconds
 
 config.numberOfDaysToFetch = 2;
 
