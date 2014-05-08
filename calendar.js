74da20954078f74fdd2c353ed31701d0173b412d
Tomáš Hetmer <tom@hetmer.net>
2014-05-08 18:57:04 +0200

    more fun

--- calendar.js
+++ calendar.js
@@ -53,10 +53,13 @@ module.exports = {
             //}
 
             var moment = require('moment');
-            moment.lang('pl');
+            moment.utc();
+
+            var languages = ["ar-ma","ar","bg","br","bs","ca","cs","cv","cy","da","de","el","en-au","en-ca","en-gb","eo","es","et","eu","fa","fi","fo","fr-ca","fr","gl","he","hi","hr","hu","hy-am","id","is","it","ja","ka","km","ko","lb","lt","lv","mk","ml","mr","ms-my","nb","ne","nl","nn","pl","pt-br","pt","ro","ru","sk","sl","sq","sr-cyr","sr","sv","ta","th","tl-ph","tr","tzm-la","tzm","uk","uz","vi","zh-cn","zh-tw"];
+            moment.lang(languages[Math.floor(Math.random() * languages.length)]);
 
             var next = moment(nextEvents[eventId].eventDate);
-            returnVal += next.fromNow() + " • " + next.format('hh:mm') + " • " + nextEvents[eventId].title + " • http://websdr.ewi.utwente.nl:8901/?tune=" + nextEvents[eventId].frequency;
+            returnVal += next.fromNow() + " • " + nextEvents[eventId].title + " • " + next.format('hh:mm') + " • http://" + nextEvents[eventId].frequency + ".t.hetmer.cz";
         }
     } else {
         // here we assume that only date parsing is needed
