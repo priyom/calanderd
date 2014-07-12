5b28c15f96cdc4cc932da0c0154074156aceb0a4
Tomáš Hetmer <TomHetmer@users.noreply.github.com>
2014-07-12 20:31:55 +0200

    allow for hidden events

--- widget.js
+++ widget.js
@@ -36,6 +36,9 @@ function getEvents() {
   
   for (var i = 0; i < obj.items.length; i++) {
     var title = obj.items[i].summary;
+    
+    if (typeof title === 'undefined') continue;
+    
     var time = obj.items[i].start.dateTime;
     var eventDate = new Date(time);
     var frequency = extractFrequency(title);
