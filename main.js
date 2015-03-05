0602fa54b5fa1409671b8317339314aeb17d8109
Daniel Ekmann <daniel@danix111.com>
2015-03-05 23:04:42 +0100

    Added all the stuff we're loving this bot for

--- main.js
+++ main.js
@@ -46,6 +46,34 @@ client.connect(5, function (input) {
 });
 
 client.addListener('message' + config.room, function (from, to, message) {
+    if(message.args[1].indexOf("!manyu ") === 0) {
+       var re = / (.*)/; 
+       var match = re.exec(message.args[1]);
+       client.say(config.room, 'Manyu hugs,' + match[0]);
+       return true;
+    }
+
+    if(message.args[1].indexOf("!veryu ") === 0) {
+       var re = / (.*)/; 
+       var match = re.exec(message.args[1]);
+       client.say(config.room, 'Veryu strong,' + match[0]);
+       return true;
+    }
+
+    if(message.args[1].indexOf("!k ") === 0 || message.args[1].indexOf("!kurva ") === 0 || message.args[1].indexOf("!f ") === 0 || message.args[1].indexOf("!fuck ") === 0) {
+       var re = / (.*)/; 
+       var match = re.exec(message.args[1]);
+       client.say(config.room, 'KURVA' + match[0] + ' !');
+       return true;
+    }
+
+    if(message.args[1].indexOf("!fast ") === 0) {
+       var re = / (.*)/; 
+       var match = re.exec(message.args[1]);
+       client.say(config.room, match[0] + ', fast!');
+       return true;
+    }
+
 
     switch(message.args[1]) {
         case '!next':
@@ -68,6 +96,20 @@ client.addListener('message' + config.room, function (from, to, message) {
             console.log('[i] restarting');
             events = [];
             break;
+        case '!manyu':
+            client.say(config.room, 'Manyu hugs, ' + from);
+            break;
+        case '!veryu':
+            client.say(config.room, 'Veryu strong, ' + from);
+            break;
+        case '!f':
+        case '!fuck':
+        case '!kurva':
+        case '!k':
+            client.say(config.room, from + ', KURVA !');
+            break;
+        case '!fast':
+            client.say(config.room, 'Voice, fast!');
         default:
             break;
     }
