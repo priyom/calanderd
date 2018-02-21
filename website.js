28fe8fdd0bd6f7ff16e04ffa1f3a802b6d12ea20
Pierre Ynard <linkfanel@yahoo.fr>
2018-02-21 20:26:21 +0100

    Reorganization of Russian military pages
    S[freq] designators are now internal legacy

--- website.js
+++ website.js
@@ -14,7 +14,7 @@ var website = module.exports = {
 	// Extra (non-station) info page links
 	extra: {
 		'meta': [ 'number', 'operators', 'russian-6', 'message-metadata' ],
-		'monolith': [ 'military', 'russia', 'monolyth-messages-description' ],
+		'monolith': [ 'military', 'russia', 'message-format' ],
 		'alphabet': [ 'military', 'russia', 'russian-phonetic-alphabet-and-numbers' ],
 		'sked': [ 'number', 'station-schedule' ],
 		'ns': [ 'number' ],
@@ -24,11 +24,10 @@ var website = module.exports = {
 		'S28': 'the-buzzer',
 		'S30': 'the-pip',
 		'S32': 'the-squeaky-wheel',
-		'XG': 'the-goose',
-		'S4020': 's4020-the-air-horn',
-		'S4110': 'zaliv-52',
-		'S4325': 'plovets-41',
-		'S6930': 'katok-65',
+		'S3012': 'the-goose',
+		'S4020': 'the-air-horn',
+		'S4524': 't-marker',
+		'S5292': 'd-marker',
 		'MXI': 'naval-markers',
 		'VC01': 'chinese-robot',
 		'XSL': 'slot-machine',
@@ -37,6 +36,6 @@ var website = module.exports = {
 	// Log pages
 	logs: {
 		month: [ 'january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december' ],
-		monthly: [ 'F06', 'S28', 'S30', 'S32', 'XG', 'S3850', 'S4020', 'S4110', 'S4467', 'S5292', 'S6930' ],
+		monthly: [ 'S28', 'S30', 'S32' ],
 	},
 };
