abc42d06dbc755c00594f44c8f0532dc72db73d4
Pierre Ynard <linkfanel@yahoo.fr>
2018-02-21 20:41:54 +0100

    Link fake traffic network page from `!fake`

--- main.js
+++ main.js
@@ -476,7 +476,7 @@ var ivo = (function() {
 					'!why': 'The Buzzer is not audible at this time of the day due to HF propagation characteristics. Try again later in the local evening.',
 					'!rivet': 'http://www.apul64.dsl.pipex.com/enigma2000/rivet/index.html',
 					'!help': 'Available commands: !n | !n [freq|freq-|freq1-freq2|station|E|G|S|V|M|F|XP|HM|SVR|GRU|DGI|poland|ukraine|>target|+minutes] (negation supported) | !logs station | !link station | !utc | !reload',
-					'!fake': 'Fake messages are messages on some E06, G06, and M14 schedules that are believed to contain no real content whatsoever due to long-period repeats (months, years) and non-random groups'
+					'!fake': 'Fake messages are messages on some E06, G06, and M14 schedules that are believed to contain no real content whatsoever due to long-period repeats (months, years) and non-random groups: http://priyom.org/number-stations/operators/russian-6/fake-traffic-network'
 				};
 
 				var info = staticInfo[cmd];
